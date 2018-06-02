215dbd86dc043b94d37decebb9bfb2ccf5341188
Alex Savvin <savvin@mail.ru>
2018-06-02 09:26:16 +0300

    Final commit 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
 	has_many :microposts
+	validates :name,  length: { maximum: 60 },  presence: true
+	validates :email, length: { maximum: 140 }, presence: true, uniqueness: true
 end
